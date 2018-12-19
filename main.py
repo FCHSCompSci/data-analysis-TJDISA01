d70cbbca7a25b0fe175325274387e013d2e96a41
TJDISA01 <taylor1011.disalvo@gmail.com>
2018-12-19 09:31:15 -0500

    Very Last Commit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import csv
 
 name = []
 abundance = []
+percentages = []
 
 def read_data():
     with open("data.csv") as data_file:
@@ -24,17 +25,27 @@ def read_data():
                 abundance.append(row_2)
                 line_count += 1
         print(f'Processed {line_count} lines.')
+    total = 0
+    for num in abundance:
+        total += num
+    for num in abundance:
+        percent = str('%.4f' % (100 * (num / total))) + "%"
+        percentages.append(percent)
     graph()
 
 def graph():
-    plt.bar(name, abundance)
+    plt.bar(name, abundance, zorder=3)
     plt.title("Abundance of Elements In Our Solar System", fontsize=20)
     plt.xlabel("Element", fontsize=18)
     plt.ylabel("Abundance", fontsize=18)
+    plt.xticks(rotation=90)
     plt.yscale("log")
-    plt.grid(True)
+    plt.grid(True, zorder=0)
     plt.margins(False)
     plt.tick_params(axis='x', labelsize=8)
+    for x, y in enumerate(abundance):
+        value = percentages[x]
+        plt.text(x - 1.25, y, str(value), fontsize=6)
     plt.show()
 
 read_data()
